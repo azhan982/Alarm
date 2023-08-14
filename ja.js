6762eef50afdd6e34490b119a8d59f7d0801a217
azzu <Azhan@willfly.com>
2023-08-14 23:15:38 +0530

    functional now

--- ja.js
+++ ja.js
@@ -2,5 +2,20 @@ function mus() {
     var audio = new Audio('http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Sevish_-__nbsp_.mp3');
     audio.play()}
 
-    mus(
 
+
+
+bt.addEventListener('click',()=>{
+    let mi = tim.value
+    pas.innerHTML = mi
+    let i = mi
+    while(i>0) {
+    pas.innerHTML = parseInt(pas.innerHTML)-1
+    i-=1}
+    fo.innerHTML =`Alarm is set for next ${mi} minutes (Refresh for Stoping Timer)`
+    tim.value = ''
+    setTimeout(()=>{
+        mus()
+    },1000 * mi)
+}
+)
